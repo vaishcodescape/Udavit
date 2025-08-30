6a924270b249c5a6dbba8122f6631311240d47ab
Aditya Vaish <adityavaish846@gmail.com>
2025-08-30 13:33:44 +0530

    Fix Expo Go compatibility: remove conflicting app router, fix configs, and clean up project structure

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,5 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { 
-  input: './global.css',
-  inlineRem: 16
+  input: './global.css'
 });
